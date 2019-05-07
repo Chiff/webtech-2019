dfeaddf0b7358ac686dff92432cb6acf2b432092
Martin Filo <martinxfilo@gmail.com>
2019-05-07 21:09:31 +0200

    csv import and read

--- public/index.php
+++ public/index.php
@@ -1,1 +1,35 @@
-<?php echo "hello world";
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: text/html; charset=utf-8');
+require_once('../src/Controller/csv-controller.php');
+
+if (isset($_POST["submit"]) && isset($_FILES["file"])) {
+    $foo = new CsvController();
+    $foo->autoPilot($_POST["delimiter"], true);
+}
+
+?>
+<!DOCTYPE html>
+<head>
+    <title>Zadanie</title>
+
+    <link rel="stylesheet" type="text/css" href="http://147.175.121.210:8153/z2/style.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+</head>
+<body>
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
+    <label>
+        Vyber CSV na import
+        <input type="file" name="file" required>
+    </label>
+    <label>
+        Delimiter
+        <input type="text" name="delimiter" value=";" required>
+    </label>
+    <input type="submit" name="submit"/>
+</form>
+</body>
---
+++ src/Controller/csv-controller.php
@@ -0,0 +1,79 @@
+<?php
+
+class CsvController {
+    private $uploadDir = '/home/xfilom/public_html/Zaver/uploaded/';
+    private $uploadFile = null;
+    private $data = array();
+
+    function autoPilot($delimiter, $verbose = false) {
+        $this->saveCSV();
+
+        if (isset($this->uploadFile)) {
+            $this->readCSV($this->uploadFile, $delimiter);
+        }
+
+        if (isset($this->data) && $verbose) {
+            echo json_encode($this->data);
+
+            return $this->data;
+        } else if ($verbose) {
+            echo "Data sa nepodarilo ziskat";
+        }
+
+        return null;
+    }
+
+    function saveCSV(): string {
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+            return null;
+        }
+
+        $uploadFile = $this->uploadDir . basename($_FILES['file']['name']);
+
+        echo "<p>";
+
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
+            $this->uploadFile = $uploadFile;
+            return $uploadFile;
+        } else {
+            echo "Upload failed";
+            return null;
+        }
+    }
+
+    function readCSV($uploadFile, $delimiter = ";") {
+        if (!($file = fopen($uploadFile, 'r')))
+            return;
+
+        $firstLine = fgets($file, 4096);
+        $itemCount = strlen($firstLine) - strlen(str_replace($delimiter, "", $firstLine)) + 1;
+
+        $fields = explode($delimiter, $firstLine, $itemCount);
+
+        $i = 0;
+        $line = array();
+        $newData = array();
+        while ($line[$i] = fgets($file, 4096)) {
+            $row = explode($delimiter, $line[$i], $itemCount);
+
+            $newData[$i] = array();
+            for ($j = 0; $j < $itemCount; $j++) {
+                $key = trim(preg_replace('/\s\s+/', '', $fields[$j]));
+                $value = trim(preg_replace('/\s\s+/', '', $row[$j]));
+                $newData[$i][$key] = $value;
+            }
+
+            $i++;
+        }
+
+        fclose($file);
+
+        $this->data = $newData;
+        return $newData;
+    }
+
+    public function getData(): array {
+        return $this->data;
+    }
+}
