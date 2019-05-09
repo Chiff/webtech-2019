36dd70750a360f39344bf443524ea6faaeae7d61
Martin Filo <martinxfilo@gmail.com>
2019-05-09 18:22:35 +0200

    rel paths correction

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../config/config.php');
+require_once(dirname(__DIR__, 1) . '/config/config.php');
 
 if ($enableDebug) {
     ini_set('display_errors', 1);
@@ -8,12 +8,15 @@ if ($enableDebug) {
 }
 
 function writeHead() {
-    echo '
-    <link rel="stylesheet" type="text/css" href="http://147.175.121.210:8153/z2/style.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="../assets/js/main.js"></script>
-    ';
+    $dir = dirname(__DIR__, 1);
+    $baseFolder = "http://" . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], "", $dir);
+
+    echo "
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://147.175.121.210:8153/z2/style.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"" . $baseFolder . "/assets/css/main.css\">
+    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+    <script src=\"" . $baseFolder . "/assets/js/main.js\"></script>
+    ";
 }
 
 function stringExists($str) {
