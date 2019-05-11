61e12fd871bb4cd856bafe5b020508e4ffaa8c0e
Martin Filo <martinxfilo@gmail.com>
2019-05-11 15:12:20 +0200

    student results api - admin

--- public/api/results.php
+++ public/api/results.php
@@ -12,16 +12,32 @@ $conn = new mysqli($hostname, $username, $password, $dbname);
 $conn->set_charset("utf8");
 
 $uid = $_SESSION["uid"];
+$where = "WHERE st.ais_id=$uid";
+
+if ($_SESSION["username"] == 'admin' && $uid == "999999999") {
+    if(!isset($_GET["subject"])) {
+        writeError(400, 'Bad Request', "Predmet musi byt zadany");
+        return;
+    }
+    $subject = $_GET["subject"];
+
+    if (!is_numeric($subject)) {
+        writeError(400, 'Bad Request', "Predmet je v nespravnom tvar: $subject");
+        return;
+    }
+
+    $where = "WHERE ss.subject_id=$subject";
+}
+
 $query = "SELECT st.ais_id, st.name, concat(su.label, ' - ',su.year) as subject, ss.id as subject_student_id 
           FROM student_subject as ss 
           JOIN student as st on st.ais_id = ss.student_id 
-          JOIN subject as su on su.id = ss.subject_id
-          WHERE st.ais_id=$uid;
-";
+          JOIN subject as su on su.id = ss.subject_id $where";
+echo $query;
 $result = $conn->query($query);
 
 if ($conn->error) {
-    writeError(400, 'Bad Request', "Chyba pri ziskavani vysledkov studenta. Warning: $conn->error");
+    writeError(400, 'Bad Request', "Chyba pri ziskavani vysledkov studenta . Warning: $conn->error");
     return;
 }
 $temp = $result->fetch_all(MYSQLI_ASSOC);
@@ -29,11 +45,11 @@ $ret = array();
 
 foreach ($temp as $item) {
     $ssid = $item['subject_student_id'];
-    $query = "SELECT label, result FROM result WHERE student_subject_id=$ssid";
+    $query = "SELECT label, result FROM result WHERE student_subject_id = $ssid";
     $result = $conn->query($query);
 
     if ($conn->error) {
-        writeError(400, 'Bad Request', "Chyba pri ziskavani vysledkov studenta. Warning: $conn->error");
+        writeError(400, 'Bad Request', "Chyba pri ziskavani vysledkov studenta . Warning: $conn->error");
         continue;
     }
     $item["resutlts"] = $result->fetch_all(MYSQLI_ASSOC);
