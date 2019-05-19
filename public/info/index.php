d28d3cd9edfbfaa439903e46af6a3901d1667513
Tangfei <dominik.simko747@gmail.com>
2019-05-19 22:33:31 +0200

    preklad server-data-generator

--- public/info/index.php
+++ public/info/index.php
@@ -28,84 +28,84 @@ require_once('../../src/helpers.php');
             <td>Navrh databazy</td>
             <td>Navrh databazy</td>
             <td>Navrh databazy</td>
-            <td>d</td>
+            <td>Uprava databazy</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Preklady</td>
             <td>Preklady</td>
-            <td>d</td>
+            <td>Preklady</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Navrh frontendu</td>
             <td>Navrh a tvorba frontendu</td>
-            <td>d</td>
+            <td>Tvorba sablony</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Admin page</td>
             <td>Admin page</td>
-            <td>d</td>
+            <td>Import csv suboru s udajmi o osobach</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Nahranie a spracovanie CSV</td>
             <td>Navrh projektovej struktury</td>
-            <td>d</td>
+            <td>Generovanie hesiel a csv na stiahnutie</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Vytvorenie studenta, timu (CSV->DB)</td>
             <td></td>
-            <td>d</td>
+            <td>Generovanie udajov pre prihlasenie na server</td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Vytvorenie zaznamov o bodoch (CSV->DB)</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>API na vypisanie projketov pre studenta</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>API na vypisanie timov pre admina</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>API na vypisanie vysledkov projektu (admin) a studnta (student)</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>API na vyhladanie predmetov a timov (combobox)</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
         <tr>
             <td>t</td>
             <td>d</td>
             <td>Vytvaranie projektov a predmetov</td>
             <td></td>
-            <td>d</td>
+            <td></td>
         </tr>
 
         </tbody>
